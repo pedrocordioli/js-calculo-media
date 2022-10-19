675b7b22ba70f8cdf51e321d6ce561aa17c336ff
Pedro Cordioli <pedrosenacmiro@hotmail.com>
2022-10-18 21:07:46 -0300

    javascript

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log("Olá, estou funcionando")
+console.log(10*2)
+console.log(230/2)
+// 
+let nome = "pedro"
+let nota1= 9
+let nota2= 7
+let nota3= 4
+let nota4= 2
+let media= (nota1 + nota2 + nota3 + nota4) / 4
+let mediaFixada = media.toFixed(1) 
+
+console.log("Olá + nome")
+console.log("Sua média foi:" + media)
